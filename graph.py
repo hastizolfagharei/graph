3d77f980024a2a74016e26bc056fdd94abe288b4
hastizolfagharei <59199956+hastizolfagharei@users.noreply.github.com>
2020-02-20 12:01:14 +0330

    added node class, graph methods

---
+++ graph.py
@@ -0,0 +1,20 @@
+from link import Link
+from node import Node
+
+class Graph:
+
+    def __init__(self):
+        n_dict = {}
+        l_link = {}
+
+    def add_node(self):
+        pass
+
+    def delete_node(self):
+        pass
+
+    def add_link(self):
+        pass
+
+    def delete_link(self):
+        pass
---
+++ run.py
@@ -0,0 +1,10 @@
+from link import Link
+
+
+def main():
+    link = Link(1, 2)
+    print(link.out_node_id)
+
+
+if __name__ == '__main__':
+    main()
