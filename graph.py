a6edaf6b640743bd025fba921f828fd80eafaadb
hastizolfagharei <59199956+hastizolfagharei@users.noreply.github.com>
2020-02-20 14:44:58 +0330

    completing add_node, add_link method

--- graph.py
+++ graph.py
@@ -4,17 +4,26 @@ from node import Node
 class Graph:
 
     def __init__(self):
-        n_dict = {}
-        l_link = {}
+        self.node_counter = 0
+        self.n_dict = {}
+        self.l_dict = {}
 
-    def add_node(self):
+    def add_node(self, input_node):
+        self.n_dict[self.node_counter] = input_node
+        self.node_counter += 1
+
+    def delete_node(self, rem_node):
         pass
 
-    def delete_node(self):
-        pass
 
-    def add_link(self):
-        pass
+
+    def add_link(self, input_link):
+        # add link to dict
+        self.l_dict[(input_link.in_node_id, input_link.out_node_id)] = input_link
+
+        # add nodes to degrees, which are connected to each other by this link
+        self.n_dict[input_link.in_node_id].degrees.append(input_link.out_node_id)
+        self.n_dict[input_link.out_node_id].degrees.append(input_link.in_node_id)
 
     def delete_link(self):
         pass
--- run.py
+++ run.py
@@ -1,9 +1,24 @@
 from link import Link
-
+from graph import Graph
+from node import Node
 
 def main():
-    link = Link(1, 2)
-    print(link.out_node_id)
+    # link = Link(1, 2)
+    # print(link.out_node_id)
+    g = Graph()
+    g2 = Graph()
+    n1 = Node((19, 4))
+    n2 = Node((2, 44))
+
+    l1 = Link(0, 1)
+
+    g.add_node(n1)
+    g.add_node(n2)
+    g.add_link(l1)
+    print(n1.degrees)
+    print(n2.degrees)
+    # print(g.node_counter)
+    # print(g.n_dict)
 
 
 if __name__ == '__main__':
